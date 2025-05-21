789e241d26f947027db4c9a6e93b9be32268b0cc
Hicks-99 <94490389+Hicks-99@users.noreply.github.com>
2025-05-21 18:52:57 +0200

    refactor Student class to use record

--- src/main/java/streamapi/Student.java
+++ src/main/java/streamapi/Student.java
@@ -3,55 +3,12 @@ package streamapi;
 import java.util.Objects;
 
 /** Represent a student. */
-public final class Student {
-    private final String name;
-    private final int cps;
-    private final Enrollment program;
-
-    /**
-     * Create a new student object.
-     *
-     * @param name Name of the new student
-     * @param cps Amount credit points
-     * @param program Enrolled in the degree programme
-     */
-    public Student(String name, int cps, Enrollment program) {
-        this.name = name;
-        this.cps = cps;
-        this.program = program;
-    }
-
-    /**
-     * Retrieve a student's name.
-     *
-     * @return Name of this student
-     */
-    public String name() {
-        return name;
-    }
-
-    /**
-     * Retrieve a student's ECTS.
-     *
-     * @return number of credit points of this student
-     */
-    public int cps() {
-        return cps;
-    }
-
-    /**
-     * Retrieve a student's study programme.
-     *
-     * @return number of credit points of this student
-     */
-    public Enrollment program() {
-        return program;
-    }
-
+public record Student(String name, int cps, Enrollment program) {
     /**
      * Check if a student is enrolled in Computer Science.
      *
-     * @return {@code true}, iff the student is enrolled in Computer Science, otherwise {@code
+     * @return {@code true}, iff the student is enrolled in Computer Science,
+     *         otherwise {@code
      *     false}
      */
     public boolean isIFM() {
@@ -60,8 +17,10 @@ public final class Student {
 
     @Override
     public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
+        if (obj == this)
+            return true;
+        if (obj == null || obj.getClass() != this.getClass())
+            return false;
         var that = (Student) obj;
         return Objects.equals(this.name, that.name)
                 && this.cps == that.cps
